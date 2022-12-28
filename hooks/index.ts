0038fe8780d73a0e5faee0b632f63d1342325e4b
brightchul <abhbtbb1@gmail.com>
2022-12-28 17:59:31 +0900

    feat: add useToggle

--- hooks/index.ts
+++ hooks/index.ts
@@ -1,8 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
+import type { TypedUseSelectorHook } from "react-redux";
+import { useCallback, useState } from "react";
 
 import { AppDispatch, RootState } from "../stores";
 
-import type { TypedUseSelectorHook } from "react-redux";
-
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export function useToggle(initialState = false): [boolean, () => void] {
+  const [state, setState] = useState(initialState);
+  const toggle = useCallback(() => setState((state) => !state), []);
+
+  return [state, toggle];
+}
